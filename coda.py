b2887ea686a0d2255c28e139a9a88b008c81ea1d
adamlenart <alenart@health.sdu.dk>
2016-09-12 17:19:05 +0200

    Added CompositionalTransform class, moved alr there

--- coda.py
+++ coda.py
@@ -1,19 +1,38 @@
 import numpy as np
+import pandas as pd
 
 class CompositionalData:
     """Compositional data are positive data with a constant sum. 
     Their statistical analysis require special consideration as, due to the 
     constant sum, compositional data define an (n-1) dimensional space where 
-    n is the number of components. Therefore, the standard Euclidean n-dimensional
-    basis does not provide an orthonormal basis in the compositional space.
-    Because of this, the proper statistical space of compositional data is the 
-    (n-1) simplex."""
+    n is the number of components. """
     
-    def __init__(self,values=None):
+    def __init__(self,values=None,index_name=None,column_name=None):
         if values is None:
-            self.values = np.array([])
+            self.values = pd.DataFrame(np.array([]))
         else:
-            self.values = np.array(values)
-        self.total = sum(self.values)
+            self.values = pd.DataFrame(np.array(values))
+        if index_name is None:
+            self.values.index = ["c{num}".format(num=number) for number in range(1,self.values.shape[0]+1)]
+        if column_name is None:
+            self.values.columns = ["p{num}".format(num=number) for number in range(1,self.values.shape[1]+1)]
+        self.total = np.sum(self.values,axis=1)
+    #closure operation    
     def close(self):
-        return self.values/self.total
+        # divide rowwise
+        return self.values.div(self.total,axis=0)
+
+    
+class CompositionalTransform:
+    
+    """The standard Euclidean n-dimensional basis does not provide an orthonormal basis in the compositional space.
+    Because of this, the proper statistical space of compositional data is the (n-1) simplex. However, 
+    compositional transformations map the composition from the (n-1) simplex to the (n-1) Euclidean space allowing the 
+    use of standard multivariate statistical techniques."""
+    
+    # additive log-ratio transform
+    def alr(compositional_data,rm_index=None):
+        if rm_index is None:
+            rm_index = compositional_data.values.columns[-1] 
+        rm_part = compositional_data.values.loc[:,rm_index]
+        return np.log(compositional_data.values.drop(rm_index,axis=1).div(rm_part,axis=0))
